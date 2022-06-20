bfffe3387e7de7064b8767b1dcf0c77c5a80c3a2
bekzod <bekzodbaratov@gmail.com>
2022-06-20 11:34:51 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/header/headerComponents";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
