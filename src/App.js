5ba476a9a565e31fa36740ac0e65d746fb44d0be
jafar <mirzaraximovjafar2001@gmail.com>
2022-06-20 11:58:47 +0500

    change images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header/headerComponents";
+import TopSellersComponents from "./components/TopSellers/TopSellersComponents";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <TopSellersComponents/>
     </div>
   );
 };
