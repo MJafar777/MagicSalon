e5c290a56a50bada25eae3380b3b639b21774d73
jafar <mirzaraximovjafar2001@gmail.com>
2022-06-20 18:50:22 +0500

    section TopSeler Finished

--- src/components/TopSellers/TopSellers.jsx
+++ src/components/TopSellers/TopSellers.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import "./TomSellers.scss";
 
 const TopSellers = () => {
   return (
     <div className="TopSeler">
       <div className="TopSeler__header">
-        <h2 className="TopSeler__header--h2">TOP SELLERS</h2>
+        <h2 className="TopSeler__header--h2">
+          <div className="TopSeler__header--h2-border"></div>TOP SELLERS
+          <div className="TopSeler__header--h2-border"></div>
+        </h2>
+        <div className="imgBackground">
+          <div className="imgBackground--ImgTop1">
+            <div className="imgBackground--ImgTop1-text">
+              <h2>Jelly Ball</h2>
+              <p>Radiance-boosting Cleanser With Lychee</p>
+            </div>
+            <button className="imgBackground--ImgTop1-btnShop">SHOP NOW</button>
+          </div>
+
+          <div className="imgBackground--ImgTop2">
+            <div className="imgBackground--ImgTop2-text">
+              <h2>Pore Patrol</h2>
+              <p>Meet your FW18 closet cornerstone</p>
+            </div>
+            <button className="imgBackground--ImgTop1-btnShop">SHOP NOW</button>
+          </div>
+        </div>
       </div>
+      <a className="TopSeler--link" href="1">
+        SEE ALL
+      </a>
     </div>
   );
 };
