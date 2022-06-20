ca75aa92aa50792cd1540c4a8ad4a9690e08f1ac
Xusniddin <narkulof@gmail.com>
2022-06-20 14:18:06 +0500

    banner completed

--- src/components/SaleBaner/Salebanner.jsx
+++ src/components/SaleBaner/Salebanner.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import React from "react";
+import "./saleBanner.scss";
 
 const Salebanner = () => {
-  return (
-    <div>
-      Salebanner
-    </div>
-  );
+   return (
+      <div className="banner">
+         <div className="banner__content">
+            <div className="banner__content--bigText">
+               <b className="banner__content--bigText-number">35</b>
+               <span className="banner__content--bigText-span">% OFF</span>
+            </div>
+            <div className="banner__content--pinkText">
+               <span className="banner__content--pinkText-span">All </span>
+               <b className="banner__content--pinkText-b">SKINCARE </b>
+               <span className="banner__content--pinkText-span">Items </span>
+            </div>
+            <button className="banner__content--btn">SHOP NOW</button>
+         </div>
+      </div>
+   );
 };
-
 
 export default Salebanner;
