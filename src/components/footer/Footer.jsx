adf6e45f2c7686ece9cbab0abc215c7bfcfccd9a
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-20 17:55:48 +0500

    footer finish

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
-
-
+import React from "react";
+import "./footer.scss";
+import img from "../../images/logo.png";
+import ff from "../../images/Group 1442.png";
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className="footer">
+      <div className="menu">
+        <div className="row row_first">
+          <img className="logo_img" src={img} />
+          <img className="img_bottom" src={ff} />
+        </div>
+        <div className="row ">
+          <h2>MIRAIH</h2>
+          <button className="menu_btn">Sell with Miraih</button>
+          <p>About Us</p>
+          <p>Contact Us</p>
+          <p>Promos</p>
+          <p>Become an Ambassador</p>
+        </div>
+        <div className="row">
+          <h2>POLICY</h2>
+          <p>Terms of Usage</p>
+          <p>Privacy Policy</p>
+          <p>Return Policy</p>
+        </div>
+        <div className="row">
+          <h2>CATEGORIES</h2>
+          <p>Skincare</p>
+          <p>Makeup</p>
+          <p>Hair Care</p>
+          <p>Bath & Body</p>
+          <p>Beauty Suppliments</p>
+        </div>
+        <div className="row">
+          <h2>SUBSCRIBE</h2>
+          <p className="subs">
+            Subscribe to our newsletter, so that you can be the first to know
+            about new offers and promotions.
+          </p>
+          <div className="form">
+            <input placeholder="Enter Email Address" type="email" />
+            <button>SUBSCRIBE</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-
 
 export default Footer;
