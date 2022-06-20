d8506d56c9c42fe73f8e93d35eef723dcd014560
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-06-20 12:04:04 +0500

    Valisher Botirov amBassaDorsni tugatdim

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AmBassaDorsComponents from "./components/AmBassaDors/AmBassaDorsComponents";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <AmBassaDorsComponents />
+    </div>
+  );
 };
 
 export default App;
