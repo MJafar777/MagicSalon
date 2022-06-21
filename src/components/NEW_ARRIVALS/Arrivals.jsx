160490433f85766c4c07d08397fa1768f65a504c
jafar <mirzaraximovjafar2001@gmail.com>
2022-06-21 16:19:53 +0500

    arrivals one element finished

--- src/components/NEW_ARRIVALS/Arrivals.jsx
+++ src/components/NEW_ARRIVALS/Arrivals.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./New_ArrIvals.scss";
 
 const Arrivals = () => {
-  return <div>New_ArrIvals</div>;
+  return (
+    <div>
+      <h2 className="ArrIvals__header--h2">
+        <div className="ArrIvals__header--h2-border"></div>
+        <div className="ArrIvals__header--h2-text">NEW ARRIVALS</div>
+        <div className="ArrIvals__header--h2-border"></div>
+      </h2>
+      <div className="count__one">
+        <div className="count__one--fearst">
+          <h2 className="count__one--fearst-h2">The coat hotlist</h2>
+          <p className="count__one--fearst-p">
+            Meet your FW18 closet cornerstone
+          </p>
+          <button className="count__one--fearst-btn">SHOP NOW</button>
+        </div>
+        <div className="count__one--secont"></div>
+      </div>
+      <div className="count__two"></div>
+    </div>
+  );
 };
 
 export default Arrivals;
