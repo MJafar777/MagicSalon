89bd7a5600028a8b31d8ec50804dc5c3c2154360
jafar <mirzaraximovjafar2001@gmail.com>
2022-06-21 16:51:14 +0500

    arrivals two element finished

--- src/components/NEW_ARRIVALS/Arrivals.jsx
+++ src/components/NEW_ARRIVALS/Arrivals.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./New_ArrIvals.scss";
+import mesauda from "./images/mesauda.png";
 
 const Arrivals = () => {
   return (
@@ -17,7 +18,24 @@ const Arrivals = () => {
           </p>
           <button className="count__one--fearst-btn">SHOP NOW</button>
         </div>
-        <div className="count__one--secont"></div>
+        <div className="count__one--secont">
+          <div className="count__one--secont-left">
+            <img
+              src={mesauda}
+              className="count__one--secont-left-img1"
+              alt="#"
+            />
+            <div className="count__one--secont-left-content">
+              <p className="count__one--secont-left-content-1">JW Anderson</p>
+              <p className="count__one--secont-left-content-2">NET-A-PORTER</p>
+            </div>
+            <p className="count__one--secont-left-prices">215 Aed</p>
+          </div>
+
+          <div className="count__one--secont-right">
+            <div className="count__one--secont-left-img2"></div>
+          </div>
+        </div>
       </div>
       <div className="count__two"></div>
     </div>
