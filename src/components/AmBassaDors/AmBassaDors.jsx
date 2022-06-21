4b9b5559c8dca5072402bf681a1d8f6948797f92
Xusniddin <narkulof@gmail.com>
2022-06-21 12:49:51 +0500

    fixed style

--- src/components/AmBassaDors/AmBassaDors.jsx
+++ src/components/AmBassaDors/AmBassaDors.jsx
@@ -9,55 +9,57 @@ import img5 from "./image/box5.png";
 import img6 from "./image/box6.png";
 
 const Card = ({ img, name, nic }) => {
-  return (
-    <>
-      <div className="cardBox">
-        <img src={img} alt={name} />
-        <div className="cardBox--description">
-          <p className="cardBox--name">{name}</p>
-          <a href="#" className="cardBox--link">
-            {nic}
-          </a>
-          <div className="cardBox--items">
-            <div className="cardBox--items__sqr"></div>
-            <div className="cardBox--items__line"></div>
-            <div className="cardBox--items__sqr"></div>
-            <div className="cardBox--items__line"></div>
-            <div className="cardBox--items__sqr"></div>
-          </div>
-          <button className="cardBox--btn">SHOP WITH ME</button>
-        </div>
-      </div>
-    </>
-  );
+   return (
+      <>
+         <div className="cardBox">
+            <div>
+               <img src={img} alt={name} />
+            </div>
+            <div className="cardBox--description">
+               <p className="cardBox--name">{name}</p>
+               <a href="#" className="cardBox--link">
+                  {nic}
+               </a>
+               <div className="cardBox--items">
+                  <div className="cardBox--items__sqr"></div>
+                  <div className="cardBox--items__line"></div>
+                  <div className="cardBox--items__sqr"></div>
+                  <div className="cardBox--items__line"></div>
+                  <div className="cardBox--items__sqr"></div>
+               </div>
+               <button className="cardBox--btn">SHOP WITH ME</button>
+            </div>
+         </div>
+      </>
+   );
 };
 
 const AmBassaDorsComponents = () => {
-  return (
-    <div>
-      <div className="main">
-        <div className="main--left">
-          <img src={verticalimg} alt="vertical images" />
-        </div>
-        <div className="main--right">
-          <div className="main--right__title">
-            <h2>AMBASSADORS</h2>
-            <div></div>
-          </div>
-          <div className="main--right__cards">
-            <Card img={img1} name={"Joel Mardinie"} nic={"@mosho"} />
-            <Card img={img2} name={"Mohamad sh"} nic={"@mosho"} />
-            <Card img={img3} name={"Logain Omran"} nic={"@lomran"} />
-          </div>
-          <div className="main--right__cards">
-            <Card img={img4} name={"Joel Mardinie"} nic={"@mosho"} />
-            <Card img={img5} name={"Mohamad sh"} nic={"@mosho"} />
-            <Card img={img6} name={"Logain Omran"} nic={"@mim_design"} />
-          </div>
-        </div>
+   return (
+      <div>
+         <div className="main">
+            <div className="main--left">
+               <img src={verticalimg} alt="vertical images" />
+            </div>
+            <div className="main--right">
+               <div className="main--right__title">
+                  <h2>AMBASSADORS</h2>
+                  <div></div>
+               </div>
+               <div className="main--right__cards">
+                  <Card img={img1} name={"Joel Mardinie"} nic={"@mosho"} />
+                  <Card img={img2} name={"Mohamad sh"} nic={"@mosho"} />
+                  <Card img={img3} name={"Logain Omran"} nic={"@lomran"} />
+               </div>
+               <div className="main--right__cards">
+                  <Card img={img4} name={"Joel Mardinie"} nic={"@mosho"} />
+                  <Card img={img5} name={"Mohamad sh"} nic={"@mosho"} />
+                  <Card img={img6} name={"Logain Omran"} nic={"@mim_design"} />
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default AmBassaDorsComponents;
