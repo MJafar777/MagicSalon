cf24bc69d56a0a3f4489e941db393b2e3fd0eca6
Xusniddin <narkulof@gmail.com>
2022-06-25 16:28:22 +0500

    category bar style and responsive style completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,39 +3,122 @@
 import React, { useState } from "react";
 
 import "./header.scss";
-import acardion from "./images/acardion.svg";
-const Header = () => {
-   const [qizil, setQizil] = useState(false);
 
+import fragrance from "./images/nav/fragrance.png";
+import face from "./images/nav/face.png";
+import lips from "./images/nav/lips.png";
+
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+const Header = () => {
+   const [value, setValue] = React.useState(0);
+
+   const handleChange = (event, newValue) => {
+      setValue(newValue);
+   };
    return (
       <div>
          <div className="Menu">
-            <a
-               onClick={() => setQizil(!qizil)}
-               className={`Menu__link Menu__link2 ${qizil ? "qizil" : ""} `}
-               href="/"
+            <a className={`Menu__link Menu__link2  `}>
+               <b>ALL BRANDS </b>
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10.791"
+                  height="6.927"
+                  viewBox="0 0 10.791 6.927"
+                  style={{ verticalAlign: "10%" }}
+               >
+                  <path
+                     id="Path_4"
+                     data-name="Path 4"
+                     d="M-.73-4.875a.432.432,0,0,0,0-.609l-1.112-1.1a.424.424,0,0,0-.6,0L-6-3.033-9.556-6.589a.424.424,0,0,0-.6,0l-1.112,1.1a.432.432,0,0,0,0,.609L-6.3.087a.424.424,0,0,0,.6,0Z"
+                     transform="translate(11.396 6.715)"
+                  />
+               </svg>
+            </a>
+            {/* <div className="Menu__links">
+               <a className="Menu__link">SKINCARE</a>
+               <a className="Menu__link">MAKE UP</a>
+               <a className="Menu__link">HAIR CARE</a>
+               <a className="Menu__link">BATH & BODY</a>
+               <a className="Menu__link">BEAUTY SUPPLEMENTS</a>
+               <a className="Menu__link">PROMOS</a>
+            </div> */}
+            <Tabs
+               className="Menu__tabs"
+               style={{ color: "#111" }}
+               value={value}
+               onChange={handleChange}
+               variant="scrollable"
+               scrollButtons="auto"
+               aria-label="scrollable auto tabs example"
             >
-               ALL BRANDS{" "}
-               <img className="Menu__link2--two" src={acardion} alt="#" />
-            </a>
-            <a className="Menu__link" href="/">
-               SKINCARE
-            </a>
-            <a className="Menu__link" href="/">
-               MAKE UP
-            </a>
-            <a className="Menu__link" href="/">
-               HAIR CARE
-            </a>
-            <a className="Menu__link" href="/">
-               BATH & BODY
-            </a>
-            <a className="Menu__link" href="/">
-               BEAUTY SUPPLEMENTS
-            </a>
-            <a className="Menu__link" href="/">
-               PROMOS
-            </a>
+               <Tab label="SKINCARE" className="tab" />
+               <Tab label="MAKE UP" className="tab" />
+               <Tab label="HAIR CARE" className="tab" />
+               <Tab label="BATH & BODY" className="tab" />
+               <Tab label="BEAUTY SUPPLEMENTS" className="tab" />
+               <Tab label="PROMOS" className="tab" />
+            </Tabs>
+            <button className="Menu__btn">SELL WITH US</button>
+         </div>
+
+         <div className="subnav">
+            <div className="subnav__left">
+               <div className="subnav__left--child">
+                  <h2>Women</h2>
+                  <div className="subnav__left--child-list">
+                     <p>Fragrance</p>
+                     <p>Face</p>
+                     <p>Lips</p>
+                     <p>Eyes</p>
+                     <p>Skincare</p>
+                     <p>Hair care</p>
+                     <p>Nail care</p>
+                     <p>Gift sets</p>
+                  </div>
+               </div>
+
+               <div className="subnav__left--child">
+                  <h2>Men</h2>
+                  <div className="subnav__left--child-list">
+                     <p>Fragrance</p>
+                     <p>Gift sets</p>
+                     <p>Bath & body</p>
+                     <p>Hair care</p>
+                  </div>
+               </div>
+
+               <div className="subnav__left--child">
+                  <h2>Brands</h2>
+                  <div className="subnav__left--child-list">
+                     <p>Benefit</p>
+                     <p>Bourjois</p>
+                     <p>Crop Natural</p>
+                     <p>Essie</p>
+                     <p>3ina</p>
+                     <p>Foreo</p>
+                     <p>Lime Crime</p>
+                     <p>Maybelline New York</p>
+                  </div>
+               </div>
+            </div>
+
+            <div className="subnav__right">
+               <div className="subnav__right--child">
+                  <img src={fragrance} />
+                  <p>Fragrance</p>
+               </div>
+               <div className="subnav__right--child">
+                  <img src={face} />
+                  <p>Face</p>
+               </div>
+               <div className="subnav__right--child">
+                  <img src={lips} />
+                  <p>Lips</p>
+               </div>
+            </div>
          </div>
 
          <div className="header">
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 import "./NavbarStyle.scss";
-import logo from "./images/logo.png";
-import search from "./images/search.png";
-import AED from "./images/AED.png";
-import language from "./images/language.png";
-import person from "./images/person.png";
-import sale from "./images/sale.png";
+// import logo from "./images/logo.png";
+// import search from "./images/search.png";
+// import AED from "./images/AED.png";
+// import language from "./images/language.png";
+// import person from "./images/person.png";
+// import sale from "./images/sale.png";
 const Navbar = () => {
-  return (
-    <div className="count">
-      <div className="Navbar">
-        <div className="Navbar__left">
-          <img className="Navbar__left--img" src={logo} alt="#" />
-        </div>
-        <div className="Navbar__right">
-          <img src={search} alt="" />
-          <img src={AED} alt="" />
-          <img src={language} alt="" />
-          <img src={person} alt="" />
-          <img src={sale} alt="" />
-        </div>
-      </div>
-    </div>
-  );
+   return <div></div>;
 };
 
 export default Navbar;
